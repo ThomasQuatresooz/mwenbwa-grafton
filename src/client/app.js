8dc0387aa4268db2a002a2c8fdfdd51a1ac0f4e8
Safiye <safiye.akdede@hotmail.com>
2020-06-03 13:34:53 +0200

    ajout de leaflet

--- src/client/app.js
+++ src/client/app.js
@@ -9,6 +9,7 @@
 import * as React from "react";
 import ReactDOM from "react-dom";
 
-import HelloWorld from "./components/hello";
+// import HelloWorld from "./components/hello";
+import LeafletMap from "./components/leaflet";
 
-ReactDOM.render(<HelloWorld />, document.querySelector("#app"));
+ReactDOM.render(<LeafletMap />, document.querySelector("#app"));
