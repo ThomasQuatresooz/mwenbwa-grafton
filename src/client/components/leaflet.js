bdd935dae76c200cb2662be4b89d5306985495c4
Safiye <safiye.akdede@hotmail.com>
2020-06-03 14:19:02 +0200

    fixed error with the map rendering

--- src/client/components/leaflet.js
+++ src/client/components/leaflet.js
@@ -1,8 +1,9 @@
 import React from "react";
 import {Map, TileLayer} from "react-leaflet";
 import "../../style.css";
+import "../../../node_modules/leaflet/dist/leaflet.css";
 
-export default function App() {
+export default function LeafletMap() {
     return (
         <Map center={[45.4, -75.7]} zoom={12}>
             <TileLayer
