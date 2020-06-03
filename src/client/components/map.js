8a7b4bff8a47b867e433068fb34d7396889a59d4
Sirlys <alicia.norga@gmail.com>
2020-06-03 16:13:17 +0200

    oops, cleaning of the map.js file

--- src/client/components/map.js
+++ src/client/components/map.js
@@ -16,17 +16,5 @@ const MapLeaflet = () => (
         />
     </Map>
 );
-// L.TileLayer.provider("Stadia.AlidadeSmooth").addTo(Map);
-
-// Stadia.AlidadeSmooth;
-
-// var Stadia_AlidadeSmooth = L.tileLayer(
-//     "https://tiles.stadiamaps.com/tiles/alidade_smooth/{z}/{x}/{y}{r}.png",
-//     {
-//         maxZoom: 20,
-//         attribution:
-//             '&copy; <a href="https://stadiamaps.com/">Stadia Maps</a>, &copy; <a href="https://openmaptiles.org/">OpenMapTiles</a> &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors',
-//     },
-// );
 
 export default MapLeaflet;
