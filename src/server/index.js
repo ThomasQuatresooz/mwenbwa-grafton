a1db32ec939b6eb5767d230d8a05fe5be6cd0361
Julien <julien.minder@gmail.com>
2020-06-04 14:26:00 +0200

    commit before merge to dev

--- src/server/index.js
+++ src/server/index.js
@@ -30,10 +30,11 @@ const bodyParser = require("body-parser");
 
 mongoose
     .connect(
-        "mongodb+srv://USER1:JV4jvvRMEN5xcGDX@cluster-mwenbwa-nsqdi.gcp.mongodb.net/test?retryWrites=true&w=majority",
+        "mongodb+srv://USER1:JV4jvvRMEN5xcGDX@cluster-mwenbwa-nsqdi.gcp.mongodb.net/Trees?retryWrites=true&w=majority",
         {
             useNewUrlParser: true,
             useUnifiedTopology: true,
+            useCreateIndex: true,
         },
     )
     .then(() => console.log("Connexion à MongoDB réussie !"))
@@ -56,27 +57,4 @@ app.use((req, res, next) => {
 
 app.use(bodyParser.json());
 
-app.get("/api/stuff", (req, res) => {
-    const stuff = [
-        {
-            _id: "oeihfzeoi",
-            title: "Mon premier objet",
-            description: "Les infos de mon premier objet",
-            imageUrl:
-                "https://cdn.pixabay.com/photo/2019/06/11/18/56/camera-4267692_1280.jpg",
-            price: 4900,
-            userId: "qsomihvqios",
-        },
-        {
-            _id: "oeihfzeomoihi",
-            title: "Mon deuxième objet",
-            description: "Les infos de mon deuxième objet",
-            imageUrl:
-                "https://cdn.pixabay.com/photo/2019/06/11/18/56/camera-4267692_1280.jpg",
-            price: 2900,
-            userId: "qsomihvqios",
-        },
-    ];
-    res.status(200).json(stuff);
-});
 module.exports = app;
