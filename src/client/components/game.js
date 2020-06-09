73ea261466cdfeaa258b45a8e9ffb01c2af7cf70
Sirlys <alicia.norga@gmail.com>
2020-06-09 12:05:40 +0200

    added menu (not finished)

--- src/client/components/game.js
+++ src/client/components/game.js
@@ -1,9 +1,11 @@
 import * as React from "react";
 import MapLeaflet from "./map";
-// import Menu from "./menu";
+import Menu from "./menu";
+require("../styles/mystyles.css");
 
 const Game = () => (
-    <div>
+    <div className={"content"}>
+        <Menu />
         <MapLeaflet />
     </div>
 );
