3ebbdcb268d89101f2daacd23b07d689bdd178d3
Julien <julien.minder@gmail.com>
2020-06-11 13:46:59 +0200

    création controlleurs login et signup

--- src/server/db/models/user-schema.js
+++ src/server/db/models/user-schema.js
@@ -1,13 +1,16 @@
 import mongoose from "mongoose";
 const Schema = mongoose.Schema;
+const uniqueValidator = require("mongoose-unique-validator");
 
 const userSchema = new Schema({
-    username: {type: String, unique: true},
+    username: {type: String, unique: true, required: true},
     email: {type: String, unique: true},
     password: String,
     totalLeaves: Number,
     color: String,
 });
+
+userSchema.plugin(uniqueValidator);
 
 const User = mongoose.model("User", userSchema);
 
