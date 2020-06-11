19754dd447469735c1a3fb4a6868d6ac35816cdc
Julien <julien.minder@gmail.com>
2020-06-11 16:07:09 +0200

    correction bug router/status et controller/user

--- src/server/db/router/status.js
+++ src/server/db/router/status.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 
-router.get("/", res => {
+router.get("/", (req, res) => {
     res.status(200).json({data: "Tout est ok !"});
 });
 
