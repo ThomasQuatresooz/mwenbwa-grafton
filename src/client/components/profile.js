c609b7c5f1581081ad1d4d94b10bdb61fa830441
Safiye <safiye.akdede@hotmail.com>
2020-06-24 12:03:34 +0200

    merge with dev

--- src/client/components/profile.js
+++ src/client/components/profile.js
@@ -1,14 +1,17 @@
 /* eslint-disable react/jsx-max-depth */
 /* eslint-disable react/button-has-type */
 import React from "react";
+import "../../lib/jscolor-2.1.0/jscolor";
+import authentification from "../../server/db/middleware/authentification";
 
 function EditProfile() {
-    // let img = document.querySelector("#profileImage");
-    // let nickname = document.querySelector("#profileNickname");
-    // let email = document.querySelector("#profileEmail");
     document
         .querySelector("#editProfile")
         .setAttribute("style", "display: none");
+    document
+        .querySelector("#normalMode")
+        .setAttribute("style", "display: none");
+    document.querySelector("#editMode").removeAttribute("style");
     document.querySelector("#saveProfile").removeAttribute("style");
     document.querySelector("#cancelProfile").removeAttribute("style");
     document.querySelector("#changeImage").removeAttribute("style");
@@ -16,6 +19,8 @@ function EditProfile() {
 
 function CloseProfile() {
     document.querySelector("#editProfile").removeAttribute("style");
+    document.querySelector("#normalMode").removeAttribute("style");
+    document.querySelector("#editMode").setAttribute("style", "display: none");
     document
         .querySelector("#saveProfile")
         .setAttribute("style", "display: none");
@@ -27,111 +32,239 @@ function CloseProfile() {
         .setAttribute("style", "display: none");
 }
 
-export default function ProfilePage(props) {
-    return (
-        <div className={props.showProfile ? "modal is-active" : "modal"}>
+export default class ProfilePage extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            currentUser: authentification,
+        };
+    }
+    render() {
+        return (
             <div
-                className={"modal-background"}
-                onClick={props.handleCloseProfile}
-            />
-            <div className={"modal-card"}>
-                <header className={"modal-card-head"}>
-                    <p className={"modal-card-title"}>{"Profile"}</p>
-                    <button
-                        className={"delete"}
-                        aria-label={"close"}
-                        onClick={props.handleCloseProfile}
-                    />
-                </header>
-                <section className={"modal-card-body has-text-centered"}>
-                    <div className={"columns is-centered"}>
-                        <div className={"column is-5"}>
-                            <img
-                                className={"is-square"}
-                                id={"profileImage"}
-                                src={
-                                    "https://bulma.io/images/placeholders/128x128.png"
-                                }
-                            />
+                className={
+                    this.props.showProfile ? "modal is-active" : "modal"
+                }>
+                <div
+                    className={"modal-background"}
+                    onClick={this.props.handleCloseProfile}
+                />
+                <div className={"modal-card"}>
+                    <header className={"modal-card-head"}>
+                        <p className={"modal-card-title"}>{"Profile"}</p>
+                        <button
+                            className={"delete"}
+                            aria-label={"close"}
+                            onClick={this.props.handleCloseProfile}
+                        />
+                    </header>
+                    <section className={"modal-card-body has-text-centered"}>
+                        <div className={"columns is-centered"}>
+                            <div className={"column is-5"}>
+                                <img
+                                    className={"is-square"}
+                                    id={"profileImage"}
+                                    src={""}
+                                />
+                                <div
+                                    className={"file is-info is-centered"}
+                                    id={"changeImage"}
+                                    style={{display: "none"}}>
+                                    <label className={"file-label"}>
+                                        <input
+                                            className={"file-input"}
+                                            type={"file"}
+                                        />
+                                        <span className={"file-cta"}>
+                                            <span className={"file-icon"}>
+                                                <i
+                                                    className={"fas fa-upload"}
+                                                />
+                                            </span>
+                                            <span className={"file-label"}>
+                                                {"Click to change"}
+                                            </span>
+                                        </span>
+                                    </label>
+                                </div>
+                            </div>
                             <div
-                                className={"file is-info is-centered"}
-                                id={"changeImage"}
-                                style={{display: "none"}}>
-                                <label className={"file-label"}>
-                                    <input
-                                        className={"file-input"}
-                                        type={"file"}
-                                    />
-                                    <span className={"file-cta"}>
-                                        <span className={"file-icon"}>
-                                            <i className={"fas fa-upload"} />
+                                id={"editMode"}
+                                style={{display: "none"}}
+                                className={"column is-5"}>
+                                <div className={"field"}>
+                                    <label className={"label"}>
+                                        {"Username"}
+                                    </label>
+                                    <div
+                                        className={
+                                            "control has-icons-left has-icons-right"
+                                        }>
+                                        <input
+                                            className={"input is-success"}
+                                            type={"text"}
+                                            placeholder={"Text input"}
+                                            value={"bulma"}
+                                        />
+                                        <span
+                                            className={"icon is-small is-left"}>
+                                            <i className={"fas fa-user"} />
                                         </span>
-                                        <span className={"file-label"}>
-                                            {"Click to change"}
+                                        <span
+                                            className={
+                                                "icon is-small is-right"
+                                            }>
+                                            <i className={"fas fa-check"} />
                                         </span>
-                                    </span>
-                                </label>
+                                    </div>
+                                    <p className={"help is-success"}>
+                                        {"This username is available"}
+                                    </p>
+                                </div>
+                                <div className={"field"}>
+                                    <label className={"label"}>{"Email"}</label>
+                                    <div
+                                        className={
+                                            "control has-icons-left has-icons-right"
+                                        }>
+                                        <input
+                                            className={"input is-danger"}
+                                            type={"email"}
+                                            placeholder={"Email input"}
+                                            value={"hello@"}
+                                        />
+                                        <span
+                                            className={"icon is-small is-left"}>
+                                            <i className={"fas fa-envelope"} />
+                                        </span>
+                                        <span
+                                            className={
+                                                "icon is-small is-right"
+                                            }>
+                                            <i
+                                                className={
+                                                    "fas fa-exclamation-triangle"
+                                                }
+                                            />
+                                        </span>
+                                    </div>
+                                    <p className={"help is-danger"}>
+                                        {"This email is invalid"}
+                                    </p>
+                                </div>
+                                <div className={"field"}>
+                                    <label className={"label"}>
+                                        {"Password"}
+                                    </label>
+                                    <p className={"control has-icons-left"}>
+                                        <input
+                                            className={"input"}
+                                            type={"password"}
+                                            placeholder={"Password"}
+                                        />
+                                        <span
+                                            className={"icon is-small is-left"}>
+                                            <i className={"fas fa-lock"} />
+                                        </span>
+                                    </p>
+                                </div>
+                                <div className={"field"}>
+                                    <label className={"label"}>
+                                        {"Confirm password"}
+                                    </label>
+                                    <p className={"control has-icons-left"}>
+                                        <input
+                                            className={"input"}
+                                            type={"password"}
+                                            placeholder={"Password"}
+                                        />
+                                        <span
+                                            className={"icon is-small is-left"}>
+                                            <i className={"fas fa-lock"} />
+                                        </span>
+                                    </p>
+                                </div>
+                                <div className={"field"}>
+                                    <label className={"label"}>
+                                        {"Pick a color"}
+                                    </label>
+                                    <p
+                                        className={
+                                            "control has-icons-left has-icons-right"
+                                        }>
+                                        <input
+                                            className={"input jscolor"}
+                                            placeholder={"Color picker"}
+                                        />
+                                        <span
+                                            className={"icon is-small is-left"}>
+                                            <i className={"fas fa-palette"} />
+                                        </span>
+                                    </p>
+                                </div>
+                            </div>
+                            <div id={"normalMode"} className={"column is-5"}>
+                                <h5
+                                    id={"profileNickname"}
+                                    className={"title is-5"}>
+                                    {""}
+                                </h5>
+                                <p id={"profileEmail"}>{""}</p>
+                                <p>{"My color : "}</p>
+                                <p>{"My ranking : "}</p>
                             </div>
                         </div>
-                        <div className={"column is-5"}>
-                            <h5 id={"profileNickname"} className={"title is-5"}>
-                                {"Starfire"}
-                            </h5>
-                            <p id={"profileEmail"}>{"Starfire@titans.com"}</p>
-                            <p>{"My color : "}</p>
-                            <p>{"My ranking : #1"}</p>
+                        <div className={"columns is-centered"}>
+                            <div className={"column is-2"}>
+                                <span className={"icon"}>
+                                    <i className={"fas fa-lg fa-tree"} />
+                                </span>
+                                <p>{""}</p>
+                            </div>
+                            <div className={"column is-2"}>
+                                <span className={"icon"}>
+                                    <i className={"fab fa-lg fa-envira"} />
+                                </span>
+                                <p>{""}</p>
+                            </div>
                         </div>
-                    </div>
-                    <div className={"columns is-centered"}>
-                        <div className={"column is-2"}>
-                            <span className={"icon"}>
-                                <i className={"fas fa-lg fa-tree"} />
-                            </span>
-                            <p>{"6"}</p>
-                        </div>
-                        <div className={"column is-2"}>
-                            <span className={"icon"}>
-                                <i className={"fab fa-lg fa-envira"} />
-                            </span>
-                            <p>{"234"}</p>
-                        </div>
-                    </div>
-                </section>
-                <footer
-                    className={"modal-card-foot"}
-                    style={{justifyContent: "center"}}>
-                    <p id={"profileButtons"} className={"buttons"}>
-                        <button
-                            className={"button is-success is-outlined"}
-                            id={"editProfile"}
-                            onClick={EditProfile}>
-                            <span className={"icon"}>
-                                <i className={"far fa-edit"} />
-                            </span>
-                            <span>{"Edit my profile"}</span>
-                        </button>
-                        <button
-                            className={"button is-success is-outlined"}
-                            id={"saveProfile"}
-                            style={{display: "none"}}>
-                            <span className={"icon is-small"}>
-                                <i className={"fas fa-check"} />
-                            </span>
-                            <span>{"Save changes"}</span>
-                        </button>
-                        <button
-                            className={"button is-danger is-outlined"}
-                            id={"cancelProfile"}
-                            style={{display: "none"}}
-                            onClick={CloseProfile}>
-                            <span>{"Cancel changes"}</span>
-                            <span className={"icon is-small"}>
-                                <i className={"fas fa-times"} />
-                            </span>
-                        </button>
-                    </p>
-                </footer>
+                    </section>
+                    <footer
+                        className={"modal-card-foot"}
+                        style={{justifyContent: "center"}}>
+                        <p id={"profileButtons"} className={"buttons"}>
+                            <button
+                                className={"button is-success is-outlined"}
+                                id={"editProfile"}
+                                onClick={EditProfile}>
+                                <span className={"icon"}>
+                                    <i className={"far fa-edit"} />
+                                </span>
+                                <span>{"Edit my profile"}</span>
+                            </button>
+                            <button
+                                className={"button is-success is-outlined"}
+                                id={"saveProfile"}
+                                style={{display: "none"}}>
+                                <span className={"icon is-small"}>
+                                    <i className={"fas fa-check"} />
+                                </span>
+                                <span>{"Save changes"}</span>
+                            </button>
+                            <button
+                                className={"button is-danger is-outlined"}
+                                id={"cancelProfile"}
+                                style={{display: "none"}}
+                                onClick={CloseProfile}>
+                                <span>{"Cancel changes"}</span>
+                                <span className={"icon is-small"}>
+                                    <i className={"fas fa-times"} />
+                                </span>
+                            </button>
+                        </p>
+                    </footer>
+                </div>
             </div>
-        </div>
-    );
+        );
+    }
 }
--- src/server/db/router/user.js
+++ src/server/db/router/user.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const router = express.Router();
-
+import auth from "../middleware/authentification";
 const userCtrl = require("../controller/user");
 
 router.post("/signup", userCtrl.signup);
 router.post("/login", userCtrl.login);
+router.post("/", userCtrl);
+router.post("/profile/:userId", auth, userCtrl);
 
 module.exports = router;
