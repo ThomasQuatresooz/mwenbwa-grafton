eaf5eacd8ca0f16ee3e0f71c5a2514bdc327e894
Safiye <safiye.akdede@hotmail.com>
2020-07-01 16:07:40 +0200

    merge gamelog update from dev-thomas

--- src/client/components/gamelog.js
+++ src/client/components/gamelog.js
@@ -1,15 +1,15 @@
 /* eslint-disable react/button-has-type */
-import React, {useState, useEffect} from "react";
+import React from "react";
 
 export default function GamelogPage(props) {
-    const [viewLogs, setViewLogs] = useState([]);
-    useEffect(() => {
-        fetch("logs/1")
-            .then(res => res.json())
-            .then(elem => {
-                setViewLogs(elem);
-            });
-    }, []);
+    // const [viewLogs, setViewLogs] = useState([]);
+    // useEffect(() => {
+    //     fetch("logs/1")
+    //         .then(res => res.json())
+    //         .then(elem => {
+    //             setViewLogs(elem);
+    //         });
+    // }, []);
 
     return (
         <div className={props.showGamelog ? "modal is-active" : "modal"}>
@@ -27,7 +27,7 @@ export default function GamelogPage(props) {
                     />
                 </header>
                 <section className={"modal-card-body has-text-centered"}>
-                    <p>{viewLogs}</p>
+                    <p>{""}</p>
                 </section>
                 <footer
                     className={"modal-card-foot"}
